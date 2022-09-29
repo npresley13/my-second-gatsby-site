abea6b74195780524f5bad0dd03cc8079c61e5d0
Nathan Presley <npresley13@gmail.com>
2022-09-29 13:33:31 -0400

    finished part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `My Second Gatsby Site`,
+    title: `Nathan Presley`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 // Step 2: Define your component
 const AboutPage = () => {
@@ -14,11 +15,5 @@ const AboutPage = () => {
 }
 
 // Step 3: Export your component
-export const Head = () => (
-    <>
-        <title>About Me</title>
-        <meta name="description" content="Your description" />
-    </>
-
-)
+export const Head = () => <Seo title="About Me" />
 export default AboutPage
